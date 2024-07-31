e558f14e8a910b51f9038f2850ddbd6eae2658c6
davarizqi <davarizqi@gmail.com>
2024-07-31 07:43:52 +0000

    [UPDATE]

---
+++ src/app/bintang/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BintangPage = () => {
+  return (
+    <div className='h-[100dvh] w-full flex items-center justify-center'>page</div>
+  )
+}
+
+export default BintangPage
