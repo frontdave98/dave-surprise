3ad7695cb350ad78e2758eeca3d2b748f9e8b788
Dava Rizqi Pradipta <davarizqi@gmail.com>
2024-08-01 06:35:48 +0700

    [UPDATE]

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  output: "export",
+  images: { unoptimized: true },
+  reactStrictMode: false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
